609ca49e5a06a68306c07f7bb257e2fcf9a65fd5
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2023-12-16 15:06:20 +0530

    Initial commit

---
+++ frontend_reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='bg-yellow-300 text-center text-3xl text-red-600'>Hello Akhilesh</h1>
+    </>
+  )
+}
+
+export default App
