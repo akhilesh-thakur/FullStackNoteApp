0d0be718f6c400756357fc98815b244f98f6e1ce
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2023-12-16 15:21:20 +0530

    testing commit

--- frontend_reactjs/src/App.jsx
+++ frontend_reactjs/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
      <h1 className='bg-yellow-300 text-center text-3xl text-red-600'>Hello Akhilesh</h1>
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis nihil exercitationem fugiat in nam. Quibusdam repellat numquam, vitae earum quidem ut obcaecati nesciunt voluptas modi fugit ab, similique culpa iste cupiditate non esse optio debitis dolore officia mollitia? Corporis blanditiis, voluptate repellat voluptates similique necessitatibus, error dolor quo ex vitae facere odit odio harum in animi omnis perferendis voluptas aut modi aspernatur. Illo hic doloribus libero quam ipsum unde exercitationem harum ad neque nihil, nobis nisi quo ab quis, voluptates molestiae impedit aperiam modi consequuntur! Quibusdam, veniam quisquam.</p>
     </>
   )
 }
