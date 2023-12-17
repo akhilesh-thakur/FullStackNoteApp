70320fe785f660650af8acf6b720614c54dc9c0d
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2023-12-17 10:04:41 +0530

    test commit

--- backend_expressjs/db.js
+++ backend_expressjs/db.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/note?appName=note";
+const mongoURI = "mongodb://localhost:27017/FullStackNewsApp";
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
   }
 
+  
+  console.log("how are you")
+
 module.exports = connectToMongo;
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
