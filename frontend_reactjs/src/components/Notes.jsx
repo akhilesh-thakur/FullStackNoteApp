c9433da7d1e25072c490ae37a65b2a108cafe8f4
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2023-12-27 17:07:12 +0530

    added CRUD operation in AddNote.jsx

--- frontend_reactjs/src/components/Notes.jsx
+++ frontend_reactjs/src/components/Notes.jsx
@@ -1,18 +1,24 @@
-import React, { useContext } from 'react';
-import NoteContext from '../context/notes/noteContext';
-import Noteitem from './Noteitem';
+import React, { useContext } from "react";
+import NoteContext from "../context/notes/noteContext";
+import Noteitem from "./Noteitem";
+import Addnote from "./Addnote";
 
 const Notes = () => {
   const context = useContext(NoteContext);
   const { notes } = context;
 
   return (
-    <div className="YourNotes">
-      <h2 className='font-bold text-2xl'>Your Notes</h2>
-      <div className='card-container'>
-      {notes.map((note) => (
-        <Noteitem key={note._id} note={note} />
-      ))}
+    <div className="parent flex justify-center ">
+      <div className="child flex justify-center flex-col">
+        <Addnote />
+        <div className="YourNotes">
+          <h2 className="font-bold text-2xl">Your Notes</h2>
+          <div className="card-container">
+            {notes.map((note) => (
+              <Noteitem key={note._id} note={note} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
